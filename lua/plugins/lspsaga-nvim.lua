9acaf01e2c8055a4ebea2e1a3c3b1418fb69ced1
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-11-18 16:51:23 +0200

    unsure if this even does anything

--- lua/plugins/lspsaga-nvim.lua
+++ lua/plugins/lspsaga-nvim.lua
@@ -19,6 +19,7 @@ return {
       -- get this icon trash outta here
       -- https://github.com/nvimdev/lspsaga.nvim/blob/4d85d4ad3ef6ba9ad0fffe478f834c29b5bfa57c/lua/lspsaga/lspkind.lua#L31
       ui = {
+        devicon = false,
         kind = {
           Folder = { " " },
           File = { "" },
