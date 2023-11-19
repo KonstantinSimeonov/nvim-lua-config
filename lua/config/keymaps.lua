4071853d47b2d8f60b11102ddd6e303d7736ad2e
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-11-19 16:19:05 +0200

    change leader and take out the garbage

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 local mapkey = vim.keymap
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Directory Navigatio}n
 mapkey.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
@@ -11,9 +11,3 @@ mapkey.set("v", "<", "<gv")
 mapkey.set("v", ">", ">gv")
 
 vim.keymap.set("n", "x", '"_x') -- delete single character without copying into register
-vim.keymap.set("n", "=", "<C-a>") -- use = to increment num
-vim.keymap.set("n", "-", "<C-x>") -- use - to decrement num
-vim.keymap.set("i", "<C-c>", "<Esc>") -- use ctrl+c for esc
-
-vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>")
