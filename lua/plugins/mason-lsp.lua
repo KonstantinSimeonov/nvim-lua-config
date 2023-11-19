34b964f630f32e68ac1f82528562d2516cf178d4
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-11-19 16:10:53 +0200

    thank you mason /s

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -1,23 +1,23 @@
 -- LANGUAGE SERVERS ...
 
 local opts = {
-	ensure_installed = {
-		"efm",
-		"bashls",
-		"tsserver",
-		"tailwindcss",
-		"html",
-		"cssls",
-		"lua_ls",
-		"jsonls",
-	},
+  ensure_installed = {
+    "efm",
+    "bashls",
+    "tsserver",
+    "tailwindcss",
+    "html",
+    "cssls",
+    "lua_ls",
+    "jsonls",
+  },
 
-	automatic_installation = true,
+  automatic_installation = true,
 }
 
 return {
-	"williamboman/mason-lspconfig.nvim",
-	opts = opts,
-	event = "BufReadPre",
-	dependencies = "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  opts = opts,
+  event = "BufReadPre",
+  dependencies = "williamboman/mason.nvim",
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,20 @@
 -- LANGUAGE SERVERS, FORMATERS, LINTERS
 
 return {
-	"williamboman/mason.nvim",
-	cmd = "Mason",
-	event = "BufReadPre",
-	opts = {},
+  "williamboman/mason.nvim",
+  lazy = false,
+  config = function()
+    local mason = require("mason")
+
+    local options = {
+      ensure_installed = { "stylua" },
+      max_concurrent_installers = 10,
+    }
+
+    mason.setup(options)
+
+    vim.api.nvim_create_user_command("MasonInstallAll", function()
+      vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
+    end, {})
+  end,
 }
