8dd80f2805bd1246ad95e51634ae03ce81280419
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-11-19 15:31:46 +0200

    yeah o is cool and all but i'm a CR peasant

--- lua/plugins/lspsaga-nvim.lua
+++ lua/plugins/lspsaga-nvim.lua
@@ -7,14 +7,16 @@ return {
 		require("lspsaga").setup({
 			-- keybinds for navigation in lspsaga window
 			move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-			-- use enter to open file with finder
-			finder_action_keys = {
-				open = "<CR>",
-			},
 			-- use enter to open file with definition preview
 			definition_action_keys = {
 				edit = "<CR>",
 			},
+
+      finder = {
+        keys = {
+          toggle_or_open = "<CR>",
+        }
+      },
 
       -- get this icon trash outta here
       -- https://github.com/nvimdev/lspsaga.nvim/blob/4d85d4ad3ef6ba9ad0fffe478f834c29b5bfa57c/lua/lspsaga/lspkind.lua#L31
