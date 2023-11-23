5e111364ce8a534958648be93485af540b037811
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-11-23 19:48:30 +0200

    add cmd and format

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,43 +1,44 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("config.options")
 require("config.keymaps")
+require("config.custom-commands")
 
 local opts = {
-	defaults = {
-		lazy = true,
-	},
-	rtp = {
-		disabled_plugins = {
-			"gzip",
-			"matchit",
-			"matchparen",
-			"netrw",
-			"netrwPlugin",
-			"tarPlugin",
-			"tohtml",
-			"tutor",
-			"zipPlugin",
-		},
-	},
-	change_detection = {
-		notify = false,
-	},
+  defaults = {
+    lazy = true,
+  },
+  rtp = {
+    disabled_plugins = {
+      "gzip",
+      "matchit",
+      "matchparen",
+      "netrw",
+      "netrwPlugin",
+      "tarPlugin",
+      "tohtml",
+      "tutor",
+      "zipPlugin",
+    },
+  },
+  change_detection = {
+    notify = false,
+  },
 }
 
 require("lazy").setup("plugins", opts)
 
 -- hacky, move elsewhere
-vim.cmd 'colorscheme panda'
+vim.cmd("colorscheme panda")
+
